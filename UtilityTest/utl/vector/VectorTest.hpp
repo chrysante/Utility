993823bc15eb52ef6346d17190d2b7df7bcecd45
chrysante <gruenlilie@protonmail.com>
2022-10-27 23:26:09 +0200

    Separated vector tests into files

---
+++ UtilityTest/utl/vector/VectorTest.hpp
@@ -0,0 +1,144 @@
+#include "LifetimeCounter.hpp"
+
+#include <utl/vector.hpp>
+
+namespace utl_test {
+
+enum class Tag;
+
+template <typename T>
+struct TagAllocator {
+    using value_type = T;
+
+    template <typename... Args>
+    void construct(T* ptr, Args&&... args) {
+        if constexpr (std::is_constructible_v<T, std::allocator_arg_t, TagAllocator, Args...>) {
+            std::construct_at(ptr, *this, std::forward<Args>(args)...);
+        }
+        else if constexpr (std::is_constructible_v<T, Args..., TagAllocator>) {
+            std::construct_at(ptr, std::forward<Args>(args)..., *this);
+        }
+        else {
+            std::construct_at(ptr, std::forward<Args>(args)...);
+        }
+    }
+
+    bool operator==(TagAllocator const&) const = default;
+
+    TagAllocator() { tag = Tag{ 0 }; }
+    TagAllocator(Tag t) { tag = t; }
+    template <typename U>
+    TagAllocator(TagAllocator<U> const& rhs) {
+        tag = rhs.tag;
+    }
+
+    T* allocate(size_t n) { return (T*)::operator new(sizeof(T) * n); }
+    void deallocate(T* ptr, size_t n) { ::operator delete(ptr); }
+
+    Tag tag;
+};
+
+struct X: utl_test::LifetimeCounter {
+    X(TagAllocator<X> alloc = {}): alloc(alloc) {}
+    X(int value, TagAllocator<X> alloc = {}): alloc(alloc), value(value) {}
+
+    X(X const& rhs, TagAllocator<X> alloc = {}): alloc(alloc), value(rhs.value) {}
+    X(X&& rhs, TagAllocator<X> alloc = {}): alloc(alloc), value(rhs.value) {}
+
+    X& operator=(X const& rhs) {
+        value = rhs.value;
+        return *this;
+    }
+    X& operator=(X&& rhs) {
+        value = rhs.value;
+        return *this;
+    }
+
+    bool operator==(X const& rhs) const { return value == rhs.value; }
+
+    TagAllocator<X> alloc;
+    int value = 0;
+
+    friend std::ostream& operator<<(std::ostream& str, X const& x) { return str << "{ " << x.value << " }"; }
+};
+
+struct TRX: X {
+    TRX(TagAllocator<TRX> alloc = {}): X(alloc) {}
+    TRX(int value, TagAllocator<TRX> alloc = {}): X(value, alloc) {}
+
+    TRX(TRX const& rhs, TagAllocator<TRX> alloc = {}): X(rhs, alloc) {}
+    TRX(TRX&& rhs, TagAllocator<TRX> alloc = {}): X(std::move(rhs), alloc) {}
+
+    TRX& operator=(TRX const& rhs) {
+        this->X::operator=(rhs);
+        return *this;
+    }
+    TRX& operator=(TRX&& rhs) {
+        this->X::operator=(std::move(rhs));
+        return *this;
+    }
+
+    bool operator==(TRX const& rhs) const { return value == rhs.value; }
+};
+
+static_assert(std::constructible_from<TRX, TRX const&, TagAllocator<TRX>>);
+
+template <typename>
+std::size_t constexpr inlineCapacity = 0;
+
+template <typename T, std::size_t N, typename A>
+std::size_t constexpr inlineCapacity<utl::small_vector<T, N, A>> = N;
+
+template <typename T>
+using TaVector = utl::vector<T, TagAllocator<T>>;
+
+template <typename T, size_t Size = utl::default_inline_capacity<T, TagAllocator<T>>>
+using TaSmallVector = utl::small_vector<T, Size, TagAllocator<T>>;
+
+} // namespace utl_test
+
+template <>
+struct utl::is_trivially_relocatable<utl_test::TRX>: std::true_type {};
+
+#define TEST_ARG_LIST(T)                                                                                               \
+    (utl_test::TaVector<T>, false), (utl_test::TaSmallVector<T>, true), (utl_test::TaSmallVector<T, 23>, true)
+
+#define PRODUCT_TEST_ARG_LIST(T)                                                                                       \
+    (utl_test::TaVector<T>, utl_test::TaVector<T>, false, false),                                                      \
+        (utl_test::TaVector<T>, utl_test::TaSmallVector<T, 0>, false, true),                                           \
+        (utl_test::TaVector<T>, utl_test::TaSmallVector<T>, false, true),                                              \
+        (utl_test::TaVector<T>, utl_test::TaSmallVector<T, 23>, false, true),                                          \
+        (utl_test::TaSmallVector<T>, utl_test::TaVector<T>, true, false),                                              \
+        (utl_test::TaSmallVector<T>, utl_test::TaSmallVector<T, 0>, true, true),                                       \
+        (utl_test::TaSmallVector<T>, utl_test::TaSmallVector<T>, true, true),                                          \
+        (utl_test::TaSmallVector<T>, utl_test::TaSmallVector<T, 23>, true, true),                                      \
+        (utl_test::TaSmallVector<T, 23>, utl_test::TaVector<T>, true, false),                                          \
+        (utl_test::TaSmallVector<T, 23>, utl_test::TaSmallVector<T, 0>, true, true),                                   \
+        (utl_test::TaSmallVector<T, 23>, utl_test::TaSmallVector<T>, true, true),                                      \
+        (utl_test::TaSmallVector<T, 23>, utl_test::TaSmallVector<T, 23>, true, true),                                  \
+        (utl_test::TaSmallVector<T, 0>, utl_test::TaVector<T>, true, false),                                           \
+        (utl_test::TaSmallVector<T, 0>, utl_test::TaSmallVector<T, 0>, true, true),                                    \
+        (utl_test::TaSmallVector<T, 0>, utl_test::TaSmallVector<T>, true, true),                                       \
+        (utl_test::TaSmallVector<T, 0>, utl_test::TaSmallVector<T, 23>, true, true)
+
+#define VECTOR_TEST_CASE(...)                UTL_VFUNC(VECTOR_TEST_CASE_, __VA_ARGS__)
+#define VECTOR_TEST_CASE_3(T, NAME, TAGS)    VECTOR_TEST_CASE_IMPL(NAME, TAGS, TEST_ARG_LIST(T))
+#define VECTOR_TEST_CASE_4(T, U, NAME, TAGS) VECTOR_TEST_CASE_IMPL(NAME, TAGS, TEST_ARG_LIST(T), TEST_ARG_LIST(U))
+
+#define VECTOR_PRODUCT_TEST_CASE(...)             UTL_VFUNC(VECTOR_PRODUCT_TEST_CASE_, __VA_ARGS__)
+#define VECTOR_PRODUCT_TEST_CASE_3(T, NAME, TAGS) VECTOR_PRODUCT_TEST_CASE_IMPL(NAME, TAGS, PRODUCT_TEST_ARG_LIST(T))
+#define VECTOR_PRODUCT_TEST_CASE_4(T, U, NAME, TAGS)                                                                   \
+    VECTOR_PRODUCT_TEST_CASE_IMPL(NAME, TAGS, PRODUCT_TEST_ARG_LIST(T), PRODUCT_TEST_ARG_LIST(U))
+
+#define VECTOR_TEST_CASE_IMPL(NAME, TAGS, ...)                                                                         \
+    TEMPLATE_TEST_CASE_SIG(NAME, TAGS, ((typename Vector, bool Small), Vector, Small), __VA_ARGS__)
+
+#define VECTOR_PRODUCT_TEST_CASE_IMPL(NAME, TAGS, ...)                                                                 \
+    TEMPLATE_TEST_CASE_SIG(NAME,                                                                                       \
+                           TAGS,                                                                                       \
+                           ((typename VectorA, typename VectorB, bool SmallA, bool SmallB),                            \
+                            VectorA,                                                                                   \
+                            VectorB,                                                                                   \
+                            SmallA,                                                                                    \
+                            SmallB),                                                                                   \
+                           __VA_ARGS__)
---
+++ UtilityTest/utl/vector/insert-erase.t.cpp
@@ -0,0 +1,373 @@
+#include "Catch2.hpp"
+
+#include <list>
+
+#include <utl/ranges.hpp>
+#include <utl/vector.hpp>
+
+#include "VectorTest.hpp"
+
+using namespace utl_test;
+
+static void reserveAdditional(auto& v, std::size_t count) {
+    auto const m = GENERATE(0u, 1u);
+    v.reserve(v.size() + count * m);
+}
+
+VECTOR_TEST_CASE(X, TRX, "vector-allocator-insert-1-2", "[vector]") {
+    using T = typename Vector::value_type;
+    X::reset();
+    Vector v({ 0, 1, 2, 3 }, Tag(1));
+    reserveAdditional(v, 1);
+    T x(-1);
+    SECTION("begin") {
+        auto const result = v.insert(v.begin(), x);
+        CHECK(result == v.begin());
+        CHECK(v == TaVector<T>{ x, 0, 1, 2, 3 });
+        CHECK(X::liveObjects() == 5 + 1);
+    }
+    SECTION("mid") {
+        auto const result = v.insert(v.begin() + 1, x);
+        CHECK(result == v.begin() + 1);
+        CHECK(v == TaVector<T>{ 0, x, 1, 2, 3 });
+        CHECK(X::liveObjects() == 5 + 1);
+    }
+    SECTION("end - 1") {
+        auto const result = v.insert(v.end() - 1, x);
+        CHECK(result == v.end() - 2);
+        CHECK(v == TaVector<T>{ 0, 1, 2, x, 3 });
+        CHECK(X::liveObjects() == 5 + 1);
+    }
+    SECTION("end") {
+        auto const result = v.insert(v.end(), x);
+        CHECK(result == v.end() - 1);
+        CHECK(v == TaVector<T>{ 0, 1, 2, 3, x });
+        CHECK(X::liveObjects() == 5 + 1);
+    }
+}
+
+VECTOR_TEST_CASE(X, TRX, "vector-allocator-insert-3", "[vector]") {
+    using T = typename Vector::value_type;
+    X::reset();
+    Vector v({ 0, 1, 2, 3 }, Tag(1));
+    T x(-1);
+    REQUIRE(X::liveObjects() == 4 + 1);
+    SECTION("into-empty") {
+        v = Vector{};
+        reserveAdditional(v, 3);
+        auto const result = v.insert(v.begin(), 3, x);
+        CHECK(result == v.begin());
+        CHECK(v == TaVector<T>{ x, x, x });
+        CHECK(X::liveObjects() == 3 + 1);
+    }
+    SECTION("into-empty/2") {
+        v = Vector{};
+        reserveAdditional(v, 1);
+        auto const result = v.insert(v.begin(), 0, x);
+        CHECK(result == v.begin());
+        CHECK(v == TaVector<T>{});
+        CHECK(X::liveObjects() == 0 + 1);
+    }
+    SECTION("begin") {
+        reserveAdditional(v, 3);
+        auto const result = v.insert(v.begin(), 3, x);
+        CHECK(result == v.begin());
+        CHECK(v == TaVector<T>{ x, x, x, 0, 1, 2, 3 });
+        CHECK(X::liveObjects() == 7 + 1);
+    }
+    SECTION("begin/2") {
+        reserveAdditional(v, 1);
+        auto const result = v.insert(v.begin(), 0, x);
+        CHECK(result == v.begin());
+        CHECK(v == TaVector<T>{ 0, 1, 2, 3 });
+        CHECK(X::liveObjects() == 4 + 1);
+    }
+    SECTION("begin/3") {
+        reserveAdditional(v, 1);
+        auto const result = v.insert(v.begin(), 1, x);
+        CHECK(result == v.begin());
+        CHECK(v == TaVector<T>{ x, 0, 1, 2, 3 });
+        CHECK(X::liveObjects() == 5 + 1);
+    }
+    SECTION("mid") {
+        reserveAdditional(v, 3);
+        auto const result = v.insert(v.begin() + 1, 3, x);
+        CHECK(result == v.begin() + 1);
+        CHECK(v == TaVector<T>{ 0, x, x, x, 1, 2, 3 });
+        CHECK(X::liveObjects() == 7 + 1);
+    }
+    SECTION("mid/2") {
+        v.push_back(T(4));
+        v.push_back(T(5));
+        reserveAdditional(v, 3);
+        auto const result = v.insert(v.begin() + 1, 3, x);
+        CHECK(result == v.begin() + 1);
+        CHECK(v == TaVector<T>{ 0, x, x, x, 1, 2, 3, 4, 5 });
+        CHECK(X::liveObjects() == 9 + 1);
+    }
+    SECTION("mid/3") {
+        reserveAdditional(v, 0);
+        auto const result = v.insert(v.begin() + 1, 0, x);
+        CHECK(result == v.begin() + 1);
+        CHECK(v == TaVector<T>{ 0, 1, 2, 3 });
+        CHECK(X::liveObjects() == 4 + 1);
+    }
+    SECTION("mid/4") {
+        reserveAdditional(v, 1);
+        auto const result = v.insert(v.begin() + 1, 1, x);
+        CHECK(result == v.begin() + 1);
+        CHECK(v == TaVector<T>{ 0, x, 1, 2, 3 });
+        CHECK(X::liveObjects() == 5 + 1);
+    }
+    SECTION("end - 1") {
+        reserveAdditional(v, 3);
+        auto const result = v.insert(v.end() - 1, 3, x);
+        CHECK(result == v.end() - 4);
+        CHECK(v == TaVector<T>{ 0, 1, 2, x, x, x, 3 });
+        CHECK(X::liveObjects() == 7 + 1);
+    }
+    SECTION("end - 1/2") {
+        reserveAdditional(v, 0);
+        auto const result = v.insert(v.end() - 1, 0, x);
+        CHECK(result == v.end() - 1);
+        CHECK(v == TaVector<T>{ 0, 1, 2, 3 });
+        CHECK(X::liveObjects() == 4 + 1);
+    }
+    SECTION("end - 1/3") {
+        reserveAdditional(v, 1);
+        auto const result = v.insert(v.end() - 1, 1, x);
+        CHECK(result == v.end() - 2);
+        CHECK(v == TaVector<T>{ 0, 1, 2, x, 3 });
+        CHECK(X::liveObjects() == 5 + 1);
+    }
+    SECTION("end - 2") {
+        reserveAdditional(v, 3);
+        auto const result = v.insert(v.end() - 2, 3, x);
+        CHECK(result == v.end() - 5);
+        CHECK(v == TaVector<T>{ 0, 1, x, x, x, 2, 3 });
+        CHECK(X::liveObjects() == 7 + 1);
+    }
+    SECTION("end") {
+        reserveAdditional(v, 3);
+        auto const result = v.insert(v.end(), 3, x);
+        CHECK(result == v.end() - 3);
+        CHECK(v == TaVector<T>{ 0, 1, 2, 3, x, x, x });
+        CHECK(X::liveObjects() == 7 + 1);
+    }
+    SECTION("end/2") {
+        reserveAdditional(v, 0);
+        auto const result = v.insert(v.end(), 0, x);
+        CHECK(result == v.end() - 0);
+        CHECK(v == TaVector<T>{ 0, 1, 2, 3 });
+        CHECK(X::liveObjects() == 4 + 1);
+    }
+    SECTION("end/3") {
+        reserveAdditional(v, 1);
+        auto const result = v.insert(v.end(), 1, x);
+        CHECK(result == v.end() - 1);
+        CHECK(v == TaVector<T>{ 0, 1, 2, 3, x });
+        CHECK(X::liveObjects() == 5 + 1);
+    }
+}
+
+VECTOR_TEST_CASE(X, TRX, "vector-allocator-insert-4", "[vector]") {
+    using T = typename Vector::value_type;
+    X::reset();
+    std::size_t const count = GENERATE(0, 3, 10);
+    Vector v({ 0, 1, 2, 3 }, Tag(1));
+    reserveAdditional(v, count);
+    std::list<T> data;
+    for (std::size_t i = 0; i < count; ++i) {
+        data.emplace_back(-(int)(i + 1));
+    }
+    auto makeRef = [&](int position) {
+        assert(position <= 4);
+        Vector result;
+        int i = 0;
+        for (; i < position; ++i) {
+            result.push_back(i);
+        }
+        for (auto& i: data) {
+            result.push_back(i);
+        }
+        for (; i < 4; ++i) {
+            result.push_back(i);
+        }
+        return result;
+    };
+    SECTION("begin") {
+        auto const result = v.insert(v.begin(), std::begin(data), std::end(data));
+        CHECK(result == v.begin());
+        CHECK(v == makeRef(0));
+        CHECK(X::liveObjects() == 4 + 2 * count);
+    }
+    SECTION("mid") {
+        auto const result = v.insert(v.begin() + 1, std::begin(data), std::end(data));
+        CHECK(result == v.begin() + 1);
+        CHECK(v == makeRef(1));
+        CHECK(X::liveObjects() == 4 + 2 * count);
+    }
+    SECTION("end - 1") {
+        auto const result = v.insert(v.end() - 1, std::begin(data), std::end(data));
+        CHECK(result == v.end() - 1 - data.size());
+        CHECK(v == makeRef(3));
+        CHECK(X::liveObjects() == 4 + 2 * count);
+    }
+    SECTION("end") {
+        auto const result = v.insert(v.end(), std::begin(data), std::end(data));
+        CHECK(result == v.end() - data.size());
+        CHECK(v == makeRef(4));
+        CHECK(X::liveObjects() == 4 + 2 * count);
+    }
+}
+
+VECTOR_TEST_CASE(X, TRX, "vector-allocator-insert-5", "[vector]") {
+    using T = typename Vector::value_type;
+    X::reset();
+    Vector v({ 0, 1, 2, 3 }, Tag(1));
+    reserveAdditional(v, 3);
+    SECTION("begin") {
+        auto const result = v.insert(v.begin(), { -1, -2, -3 });
+        CHECK(result == v.begin());
+        CHECK(v == TaVector<T>{ -1, -2, -3, 0, 1, 2, 3 });
+        CHECK(X::liveObjects() == 7);
+    }
+    SECTION("begin/2") {
+        auto const result = v.insert(v.begin(), {});
+        CHECK(result == v.begin());
+        CHECK(v == TaVector<T>{ 0, 1, 2, 3 });
+        CHECK(X::liveObjects() == 4);
+    }
+    SECTION("mid") {
+        auto const result = v.insert(v.begin() + 1, { -1, -2, -3 });
+        CHECK(result == v.begin() + 1);
+        CHECK(v == TaVector<T>{ 0, -1, -2, -3, 1, 2, 3 });
+        CHECK(X::liveObjects() == 7);
+    }
+    SECTION("mid/2") {
+        auto const result = v.insert(v.begin() + 1, {});
+        CHECK(result == v.begin() + 1);
+        CHECK(v == TaVector<T>{ 0, 1, 2, 3 });
+        CHECK(X::liveObjects() == 4);
+    }
+    SECTION("end - 1") {
+        auto const result = v.insert(v.end() - 1, { -1, -2, -3 });
+        CHECK(result == v.end() - 4);
+        CHECK(v == TaVector<T>{ 0, 1, 2, -1, -2, -3, 3 });
+        CHECK(X::liveObjects() == 7);
+    }
+    SECTION("end - 1/2") {
+        auto const result = v.insert(v.end() - 1, {});
+        CHECK(result == v.end() - 1);
+        CHECK(v == TaVector<T>{ 0, 1, 2, 3 });
+        CHECK(X::liveObjects() == 4);
+    }
+    SECTION("end") {
+        auto const result = v.insert(v.end(), { -1, -2, -3 });
+        CHECK(result == v.end() - 3);
+        CHECK(v == TaVector<T>{ 0, 1, 2, 3, -1, -2, -3 });
+        CHECK(X::liveObjects() == 7);
+    }
+    SECTION("end/2") {
+        auto const result = v.insert(v.end(), {});
+        CHECK(result == v.end());
+        CHECK(v == TaVector<T>{ 0, 1, 2, 3 });
+        CHECK(X::liveObjects() == 4);
+    }
+}
+
+VECTOR_TEST_CASE(X, TRX, "vector-erase", "[vector]") {
+    X::reset();
+    Vector v = { 0, 1, 2, 3, 4, 5 };
+    REQUIRE(X::liveObjects() == 6);
+    SECTION("all") {
+        auto result = v.erase(v.begin(), v.end());
+        CHECK(result == v.end());
+        CHECK(v.empty());
+        CHECK(X::liveObjects() == 0);
+    }
+    SECTION("begin single") {
+        auto result = v.erase(v.begin());
+        CHECK(result == v.begin());
+        CHECK(v.size() == 5);
+        CHECK(X::liveObjects() == 5);
+        CHECK(v == Vector{ 1, 2, 3, 4, 5 });
+    }
+    SECTION("end single") {
+        auto result = v.erase(v.end() - 1);
+        CHECK(result == v.end());
+        CHECK(v.size() == 5);
+        CHECK(X::liveObjects() == 5);
+        CHECK(v == Vector{ 0, 1, 2, 3, 4 });
+    }
+    SECTION("mid single") {
+        auto result = v.erase(v.begin() + 1);
+        CHECK(result == v.begin() + 1);
+        CHECK(v.size() == 5);
+        CHECK(X::liveObjects() == 5);
+        CHECK(v == Vector{ 0, 2, 3, 4, 5 });
+    }
+    SECTION("begin range") {
+        auto result = v.erase(v.begin(), v.begin() + 2);
+        CHECK(result == v.begin());
+        CHECK(v.size() == 4);
+        CHECK(X::liveObjects() == 4);
+        CHECK(v == Vector{ 2, 3, 4, 5 });
+    }
+    SECTION("end range") {
+        auto result = v.erase(v.begin() + 3, v.end());
+        CHECK(result == v.end());
+        CHECK(v.size() == 3);
+        CHECK(X::liveObjects() == 3);
+        CHECK(v == Vector{ 0, 1, 2 });
+    }
+    SECTION("mid range") {
+        auto result = v.erase(v.begin() + 1, v.end() - 2);
+        CHECK(result == v.begin() + 1);
+        CHECK(v.size() == 3);
+        CHECK(X::liveObjects() == 3);
+        CHECK(v == Vector{ 0, 4, 5 });
+    }
+    SECTION("empty range") {
+        auto result = v.erase(v.begin(), v.begin());
+        CHECK(result == v.begin());
+        CHECK(v.size() == 6);
+        CHECK(X::liveObjects() == 6);
+        CHECK(v == Vector{ 0, 1, 2, 3, 4, 5 });
+    }
+    SECTION("empty range/2") {
+        auto result = v.erase(v.begin() + 1, v.begin() + 1);
+        CHECK(result == v.begin() + 1);
+        CHECK(v.size() == 6);
+        CHECK(X::liveObjects() == 6);
+        CHECK(v == Vector{ 0, 1, 2, 3, 4, 5 });
+    }
+    SECTION("empty range/3") {
+        auto result = v.erase(v.end(), v.end());
+        CHECK(result == v.end());
+        CHECK(v.size() == 6);
+        CHECK(X::liveObjects() == 6);
+        CHECK(v == Vector{ 0, 1, 2, 3, 4, 5 });
+    }
+    SECTION("one-elem-empty range") {
+        auto result = v.erase(v.begin(), v.begin() + 1);
+        CHECK(result == v.begin());
+        CHECK(v.size() == 5);
+        CHECK(X::liveObjects() == 5);
+        CHECK(v == Vector{ 1, 2, 3, 4, 5 });
+    }
+    SECTION("one-elem-empty range/2") {
+        auto result = v.erase(v.begin() + 1, v.begin() + 2);
+        CHECK(result == v.begin() + 1);
+        CHECK(v.size() == 5);
+        CHECK(X::liveObjects() == 5);
+        CHECK(v == Vector{ 0, 2, 3, 4, 5 });
+    }
+    SECTION("one-elem-empty range/3") {
+        auto result = v.erase(v.end() - 1, v.end());
+        CHECK(result == v.end());
+        CHECK(v.size() == 5);
+        CHECK(X::liveObjects() == 5);
+        CHECK(v == Vector{ 0, 1, 2, 3, 4 });
+    }
+}
