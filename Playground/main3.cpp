f373f560b67004ae8e992e794e54d115132ff4ff
chrysante <gruenlilie@protonmail.com>
2023-01-31 17:34:44 +0100

    ..

--- Playground/main3.cpp
+++ Playground/main3.cpp
@@ -1,25 +1,36 @@
 #include <iostream>
 
-#include <utl/vector.hpp>
-#include <utl/ilist.hpp>
+#include <utl/variant.hpp>
 
-struct X: utl::ilist_node<X> {
-    X(int i): i(i) {}
+namespace {
+
+struct Base {
     int i;
 };
 
+int& f(Base& b) { return b.i; }
+
+struct A: Base {
+    A(int i, float a): Base{ i }, a(a) {}
+    float a;
+};
+
+struct B: Base {
+    B(int i, long a): Base{ i }, a(a) {}
+    long a;
+};
+
+struct X { int _; };
+
+struct C: X, B {
+    using B::B;
+};
+
+} // namespace
+
+void line() { std::cout << "====================================================================\n"; }
+
 [[gnu::weak]]
 int main() {
-    utl::ilist<X> l;
-    l.push_back(X{-1});
     
-    int const n = 11;
-    
-    for (int i = 1; i < n; ++i) {
-        l.push_front(X{ i });
-    }
-    
-    for (auto& x: l) {
-        std::cout << x.i << std::endl;
-    }
 }
