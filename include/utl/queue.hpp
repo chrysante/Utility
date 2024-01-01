94a3dd6e1e762e688c656e4b00ccb243512b4c42
chrysante <gruenlilie@protonmail.com>
2024-01-01 12:25:05 +0100

    ...

---
+++ include/utl/queue.hpp
@@ -0,0 +1,12 @@
+//
+//  queue.hpp
+//  utility
+//
+//  Created by indigo on 30.12.23.
+//
+
+#ifndef queue_h
+#define queue_h
+
+
+#endif /* queue_h */
---
+++ test/utl/queue.t.cpp
@@ -0,0 +1,8 @@
+//
+//  queue.t.cpp
+//  utl-test-all
+//
+//  Created by indigo on 30.12.23.
+//
+
+#include <stdio.h>
