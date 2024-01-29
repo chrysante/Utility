480bd301b31c1533a29995379c3dd3df22b799c3
chrysante <gruenlilie@protonmail.com>
2024-01-29 23:02:38 +0100

    Fixed int pointer pair

--- include/utl/ipp.hpp
+++ include/utl/ipp.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <concepts>
 #include <bit>
+#include <concepts>
 
 #include "__base.hpp"
 #include "__debug.hpp"
@@ -10,96 +10,90 @@ namespace utl {
 
 /// Int pointer pair
 /// Stores the integer in the low bits of the pointer.
-/// Size of the integer depends on the alignment of `T`
-template <typename T, bool Signed = false>
-struct ipp;
+///
+/// \param Pointer Any pointer type. Alignment of pointee type must be at least
+/// `2 ** NumIntBits`. The alignment requirement is not checked at compile time
+/// to allow the pointee type to be incomplete
+///
+/// \param IntType Must be unsigned integer or enum type. No sign extension is
+/// performed when loading the integer
+///
+/// \param NumIntBits The number of lower bits of the pointer that are reserved
+/// for storing the integer. Every pointer stored in an instance of this class
+/// must satisfy alignment requirement accordingly
+template <typename Pointer, typename IntType, size_t NumIntBits>
+class ipp;
 
-/// Signed integer pointer pair
-template <typename T>
-using sipp = ipp<T, true>;
+template <typename Pointee, typename IntType, size_t NumIntBits>
+class ipp<Pointee*, IntType, NumIntBits> {
+public:
+    static_assert(std::is_unsigned_v<IntType> || std::is_enum_v<IntType>,
+                  "See documentation");
 
-template <typename T, bool Signed>
-struct ipp<T*, Signed> {
-    using int_type = std::conditional_t<Signed, std::intptr_t, std::uintptr_t>;
-    
-    using pointer_type = T*;
-    
-    using __uint_type = std::make_unsigned_t<int_type>;
-    
-    static constexpr std::size_t __num_int_bits = std::countr_zero(alignof(T));
-    static constexpr __uint_type __ptr_mask = ~__uint_type(0) << __num_int_bits;
-    static constexpr __uint_type __int_mask = ~__ptr_mask;
-    static constexpr __uint_type __high_bit_mask = (__ptr_mask >> 1) & __int_mask;
-    
-    static constexpr int_type max_int() {
-        if constexpr (Signed) {
-            return (int_type)(__int_mask >> 1);
-        }
-        else {
-            return __int_mask;
-        }
-    }
-    
-    static constexpr int_type min_int() {
-        if constexpr (Signed) {
-            return (int_type)(__ptr_mask | __high_bit_mask);
-        }
-        else {
-            return 0;
-        }
-    }
-    
+    using pointer_type = Pointee*;
+
+    using int_type = IntType;
+
+    using raw_int_type = std::uintptr_t;
+
+    static constexpr size_t num_int_bits = NumIntBits;
+
+    static constexpr raw_int_type pointer_mask = ~raw_int_type(0)
+                                                 << num_int_bits;
+    static constexpr raw_int_type int_mask = ~pointer_mask;
+    static constexpr raw_int_type high_bit_mask =
+        (pointer_mask >> 1) & int_mask;
+
+    /// \Returns the largest integer value that can be stored in this class
+    static constexpr raw_int_type max_int() { return int_mask; }
+
+    /// \Returns the smallest integer value that can be stored in this class
+    static constexpr raw_int_type min_int() { return 0; }
+
+    /// Sets both values to zero
     ipp() = default;
-    
+
+    /// Construct from an integer and a pointer
     constexpr ipp(int_type value, pointer_type ptr = nullptr) {
         set_integer(value);
         set_pointer(ptr);
     }
-    
-    constexpr ipp(pointer_type ptr) {
-        __ptr = nullptr;
-        set_pointer(ptr);
-    }
-    
+
+    /// Construct from a pointer and set the integer value to zero
+    constexpr ipp(pointer_type ptr): ipp(int_type{}, ptr) {}
+
     /// Access the integer stored in the pair
     constexpr int_type integer() const {
-        if constexpr (Signed) {
-            return (int_type)(__get_int() | ((int_type)__get_int() & __high_bit_mask ? __ptr_mask : 0));
-        }
-        else {
-            return __get_int();
-        }
+        return (int_type)((raw_int_type)_m_ptr & int_mask);
     }
-    
+
     /// Access the pointer stored in the pair
     constexpr pointer_type pointer() const {
-        return (pointer_type)((__uint_type)__ptr & __ptr_mask);
+        return (pointer_type)((raw_int_type)_m_ptr & pointer_mask);
     }
-    
+
     /// Set the stored integer to \p value
     constexpr void set_integer(int_type value) {
-        __utl_assert(value >= min_int());
-        __utl_assert(value <= max_int());
-        __ptr = (pointer_type)((__uint_type)value | (__uint_type)pointer());
+        __utl_assert((raw_int_type)value >= min_int());
+        __utl_assert((raw_int_type)value <= max_int());
+        _m_ptr = (pointer_type)((raw_int_type)value | (raw_int_type)pointer());
     }
-    
+
     /// Set the stored pointer to \p ptr
     constexpr void set_pointer(pointer_type ptr) {
-        __utl_assert(std::countr_zero((__uint_type)ptr) >= __num_int_bits);
-        __ptr = (pointer_type)((__uint_type)__get_int() | (__uint_type)ptr);
+        __utl_assert(std::countr_zero((raw_int_type)ptr) >= num_int_bits &&
+                     "Invalidly aligned pointer");
+        _m_ptr = (pointer_type)((raw_int_type)integer() | (raw_int_type)ptr);
     }
-    
-    constexpr __uint_type __get_int() const {
-        return (__uint_type)__ptr & __int_mask;
-    }
-    
+
     constexpr bool operator==(ipp const& rhs) const = default;
-    
-    pointer_type __ptr;
+
+private:
+    pointer_type _m_ptr = nullptr;
 };
 
-template <size_t I, typename T, bool Signed>
-constexpr auto get(ipp<T*, Signed> p) {
+template <size_t I, typename Pointer, typename IntType, size_t NumBits>
+constexpr auto get(ipp<Pointer, IntType, NumBits> p) {
     if constexpr (I == 0) {
         return p.integer();
     }
@@ -109,82 +103,18 @@ constexpr auto get(ipp<T*, Signed> p) {
     }
 }
 
-/// Enum pointer pair
-template <typename Enum, typename Pointer>
-requires std::is_enum_v<Enum> && std::is_pointer_v<Pointer>
-struct epp {
-    static constexpr bool __Signed = std::is_signed_v<std::underlying_type_t<Enum>>;
-    
-    using enum_type = Enum;
-    
-    using int_type = typename ipp<Pointer, __Signed>::int_type;
-    
-    using pointer_type = typename ipp<Pointer, __Signed>::pointer_type;
-    
-    epp() = default;
-    
-    constexpr epp(enum_type value, pointer_type ptr = nullptr): __impl((int_type)value, ptr) {}
-    
-    constexpr epp(pointer_type ptr): __impl(ptr) {}
-    
-    /// Access the integer stored in the pair
-    constexpr enum_type enum_value() const {
-        return (enum_type)__impl.integer();
-    }
-    
-    /// Access the pointer stored in the pair
-    constexpr pointer_type pointer() const {
-        return __impl.pointer();
-    }
-    
-    /// Set the stored integer to \p value
-    constexpr void set_enum(enum_type value) {
-        __impl.set_integer((int_type)value);
-    }
-    
-    /// Set the stored pointer to \p ptr
-    constexpr void set_pointer(pointer_type ptr) {
-        __impl.set_pointer(ptr);
-    }
-    
-    constexpr bool operator==(epp const& rhs) const = default;
-    
-    ipp<pointer_type, __Signed> __impl;
-};
-
-template <size_t I, typename E, typename P>
-constexpr auto get(epp<E, P> p) {
-    if constexpr (I == 0) {
-        return p.enum_value();
-    }
-    else {
-        static_assert(I == 1);
-        return p.pointer();
-    }
-}
-
 } // namespace utl
 
-template <typename T, bool Signed>
-struct std::tuple_size<utl::ipp<T*, Signed>>:
+template <typename Pointer, typename IntType, size_t NumBits>
+struct std::tuple_size<utl::ipp<Pointer, IntType, NumBits>>:
     std::integral_constant<std::size_t, 2> {};
 
-template <typename T, bool Signed>
-struct std::tuple_element<0, utl::ipp<T*, Signed>>:
-    std::type_identity<typename utl::ipp<T*, Signed>::int_type> {};
+template <typename Pointer, typename IntType, size_t NumBits>
+struct std::tuple_element<0, utl::ipp<Pointer, IntType, NumBits>>:
+    std::type_identity<typename utl::ipp<Pointer, IntType, NumBits>::int_type> {
+};
 
-template <typename T, bool Signed>
-struct std::tuple_element<1, utl::ipp<T*, Signed>>:
-    std::type_identity<typename utl::ipp<T*, Signed>::pointer_type> {};
-
-template <typename E, typename P>
-struct std::tuple_size<utl::epp<E, P>>:
-    std::integral_constant<std::size_t, 2> {};
-
-template <typename E, typename P>
-struct std::tuple_element<0, utl::epp<E, P>>:
-    std::type_identity<typename utl::epp<E, P>::enum_type> {};
-
-template <typename E, typename P>
-struct std::tuple_element<1, utl::epp<E, P>>:
-    std::type_identity<typename utl::epp<E, P>::pointer_type> {};
+template <typename Pointer, typename IntType, size_t NumBits>
+struct std::tuple_element<1, utl::ipp<Pointer, IntType, NumBits>>:
+    std::type_identity<
+        typename utl::ipp<Pointer, IntType, NumBits>::pointer_type> {};
--- test/utl/ipp.t.cpp
+++ test/utl/ipp.t.cpp
@@ -14,35 +14,21 @@ struct __static_printer;
 
 } // namespace
 
-#define concat(a, b) concat_impl(a, b)
+#define concat(a, b)      concat_impl(a, b)
 #define concat_impl(a, b) a##b
 
-#define static_print(value) __static_printer<value> concat(__print_var, __LINE__)
-
-static_assert(utl::sipp<int16_t*>::min_int() == -1);
-static_assert(utl::sipp<int16_t*>::max_int() == 0);
-static_assert(utl::sipp<int32_t*>::min_int() == -2);
-static_assert(utl::sipp<int32_t*>::max_int() == 1);
-static_assert(utl::sipp<int64_t*>::min_int() == -4);
-static_assert(utl::sipp<int64_t*>::max_int() == 3);
-
-static_assert(utl::ipp<int16_t*>::min_int() == 0);
-static_assert(utl::ipp<int16_t*>::max_int() == 1);
-static_assert(utl::ipp<int32_t*>::min_int() == 0);
-static_assert(utl::ipp<int32_t*>::max_int() == 3);
-static_assert(utl::ipp<int64_t*>::min_int() == 0);
-static_assert(utl::ipp<int64_t*>::max_int() == 7);
-
+#define static_print(value)                                                    \
+    __static_printer<value> concat(__print_var, __LINE__)
 
 TEST_CASE("ipp aggregate init") {
-    utl::ipp<int*> p{};
+    utl::ipp<int*, unsigned, 2> p{};
     CHECK(p.pointer() == nullptr);
     CHECK(p.integer() == 0);
 }
 
 TEST_CASE("ipp") {
     SECTION("int") {
-        utl::ipp<int*> p = 3;
+        utl::ipp<int*, unsigned, 2> p = 3;
         CHECK(p.pointer() == nullptr);
         CHECK(p.integer() == 3);
         p.set_integer(1);
@@ -55,27 +41,27 @@ TEST_CASE("ipp") {
     }
     SECTION("pointer constructor") {
         Object<8> obj;
-        utl::ipp<Object<8>*> p = &obj;
+        utl::ipp<Object<8>*, unsigned, 3> p = &obj;
         CHECK(p.pointer() == &obj);
         CHECK(p.integer() == 0);
     }
     SECTION("int pointer constructor") {
         Object<8> obj;
-        utl::ipp<Object<8>*> p = { 7, &obj };
+        utl::ipp<Object<8>*, unsigned, 3> p = { 7, &obj };
         CHECK(p.pointer() == &obj);
         CHECK(p.integer() == 7);
     }
     SECTION("extended alignment type") {
         Object<16> obj;
-        utl::ipp<Object<16>*> p = { 15, &obj };
+        utl::ipp<Object<16>*, unsigned, 4> p = { 15, &obj };
         CHECK(p.pointer() == &obj);
         CHECK(p.integer() == 15);
     }
     SECTION("signed ipp") {
         Object<16> obj;
-        utl::sipp<Object<16>*> p = { -8, &obj };
+        utl::ipp<Object<16>*, unsigned, 4> p = { 15, &obj };
         CHECK(p.pointer() == &obj);
-        CHECK(p.integer() == -8);
+        CHECK(p.integer() == 15);
         p.set_integer(7);
         CHECK(p.integer() == 7);
     }
@@ -83,22 +69,34 @@ TEST_CASE("ipp") {
 
 namespace {
 
-enum class E {
-    One, Two, Three
-};
+enum class E { One, Two, Three };
 
 } // namespace
 
-TEST_CASE("epp") {
-    long x;
-    utl::epp<E, long*> p = { E::Three, &x };
-    CHECK(p.enum_value() == E::Three);
+TEST_CASE("ipp with enum") {
+    uint64_t x;
+    utl::ipp<uint64_t*, E, 3> p = { E::Three, &x };
+    CHECK(p.integer() == E::Three);
     CHECK(p.pointer() == &x);
-    p.set_enum(E::One);
-    CHECK(p.enum_value() == E::One);
+    p.set_integer(E::One);
+    CHECK(p.integer() == E::One);
     CHECK(p.pointer() == &x);
     auto [e, q] = p;
     CHECK(e == E::One);
     CHECK(q == &x);
 }
 
+namespace {
+
+struct Incomplete;
+
+} // namespace
+
+TEST_CASE("ipp with incomplete pointee type") {
+    auto* ptr = reinterpret_cast<Incomplete*>(0xDEAD'BEEF'0);
+    utl::ipp<Incomplete*, E, 1> p = { E::Two, ptr };
+    CHECK(p.integer() == E::Two);
+    CHECK(p.pointer() == ptr);
+    p.set_integer(E::One);
+    CHECK(p.integer() == E::One);
+}
