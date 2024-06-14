d9887d308cadb84969fe960f8cea6450f080bd9e
chrysante <gruenlilie@protonmail.com>
2024-06-14 17:21:24 +0100

    Fixed VFUNC macro for MSVC

--- include/utl/common.hpp
+++ include/utl/common.hpp
@@ -23,8 +23,6 @@ _UTL_SYSTEM_HEADER_
 
 /// MARK: UTL_VFUNC
 
-#define _UTL_NARG(...)   _UTL_NARG_I(__VA_ARGS__, _UTL_RSEQ_N())
-#define _UTL_NARG_I(...) _UTL_ARG_N(__VA_ARGS__)
 #define _UTL_ARG_N(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13,     \
                    _14, _15, _16, _17, _18, _19, _20, _21, _22, _23, _24, _25, \
                    _26, _27, _28, _29, _30, _31, _32, _33, _34, _35, _36, _37, \
@@ -32,11 +30,33 @@ _UTL_SYSTEM_HEADER_
                    _50, _51, _52, _53, _54, _55, _56, _57, _58, _59, _60, _61, \
                    _62, _63, N, ...)                                           \
     N
+
+#ifdef _MSC_VER
+
+#define _UTL_MSVC_CALL(X, Y)  X Y
+#define _UTL_MSVC_EXPAND(...)  __VA_ARGS__
+#define _UTL_NARG_I(...) \
+    _UTL_MSVC_EXPAND(                                                          \
+        _UTL_ARG_N(__VA_ARGS__, 63, 62, 61, 60, 59, 58, 57, 56,   \
+                                55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45,    \
+                                44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34,    \
+                                33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23,    \
+                                22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12,    \
+                                11, 10, 9,  8,  7,  6,  5,  4,  3,  2,  1, 0))
+#define _UTL_NARG(...) _UTL_MSVC_CALL(_UTL_NARG_I, (__VA_OPT__(,) __VA_ARGS__))
+
+#else
+
+#define _UTL_NARG(...)   _UTL_NARG_I(__VA_ARGS__, _UTL_RSEQ_N())
+#define _UTL_NARG_I(...) _UTL_ARG_N(__VA_ARGS__)
 #define _UTL_RSEQ_N()                                                          \
     63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46,    \
         45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29,    \
         28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12,    \
         11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
+
+#endif
+
 #define _UTL_VFUNC_IMPL(name, n) UTL_CONCAT(name, n)
 #define UTL_VFUNC(func, ...)                                                   \
     _UTL_VFUNC_IMPL(func, _UTL_NARG(__VA_ARGS__))(__VA_ARGS__)
