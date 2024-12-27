0f2250390a71982fa5b94b9f6f7254175a54d114
chrysante <gruenlilie@protonmail.com>
2024-12-27 20:30:04 +0100

    Added allocator support for hash tables

--- include/utl/hashtable.hpp
+++ include/utl/hashtable.hpp
@@ -12,19 +12,23 @@ _UTL_SYSTEM_HEADER_
 namespace utl {
 
 template <typename Key, typename Hash = utl::hash<Key>,
-          typename KeyEqual = std::equal_to<>>
-using hashset = ankerl::unordered_dense::set<Key, Hash, KeyEqual>;
+          typename KeyEqual = std::equal_to<>,
+          typename Allocator = std::allocator<Key>>
+using hashset = ankerl::unordered_dense::set<Key, Hash, KeyEqual, Allocator>;
 
 template <typename Key, typename T, typename Hash = utl::hash<Key>,
-          typename KeyEqual = std::equal_to<>>
-using hashmap = ankerl::unordered_dense::map<Key, T, Hash, KeyEqual>;
+          typename KeyEqual = std::equal_to<>,
+          typename Allocator = std::allocator<std::pair<Key, T>>>
+using hashmap = ankerl::unordered_dense::map<Key, T, Hash, KeyEqual, Allocator>;
 
 template <typename Key, typename Hash = utl::hash<Key>,
-          typename KeyEqual = std::equal_to<>>
-using node_hashset = std::unordered_set<Key, Hash, KeyEqual>;
+          typename KeyEqual = std::equal_to<>,
+          typename Allocator = std::allocator<Key>>
+using node_hashset = std::unordered_set<Key, Hash, KeyEqual, Allocator>;
 
 template <typename Key, typename T, typename Hash = utl::hash<Key>,
-          typename KeyEqual = std::equal_to<>>
-using node_hashmap = std::unordered_map<Key, T, Hash, KeyEqual>;
+          typename KeyEqual = std::equal_to<>,
+          typename Allocator = std::allocator<std::pair<Key, T>>>
+using node_hashmap = std::unordered_map<Key, T, Hash, KeyEqual, Allocator>;
 
 } // namespace utl
