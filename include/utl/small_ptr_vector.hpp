fe7bbd3ee1f8da53e31e4f6dffedd6cdd46f11d2
chrysante <gruenlilie@protonmail.com>
2025-01-07 09:22:55 +0100

    fix: Add missing include guard

--- include/utl/small_ptr_vector.hpp
+++ include/utl/small_ptr_vector.hpp
@@ -1,3 +1,6 @@
+#ifndef UTL_SMALL_PTR_VECTOR_H_
+#define UTL_SMALL_PTR_VECTOR_H_
+
 #include <utl/tiny_ptr_vector.hpp>
 
 /// This file exists for compatibility reasons
@@ -8,3 +11,5 @@ template <typename T, typename Allocator = std::allocator<T>>
 using small_ptr_vector = tiny_ptr_vector<T, Allocator>;
 
 }
+
+#endif UTL_SMALL_PTR_VECTOR_H_
