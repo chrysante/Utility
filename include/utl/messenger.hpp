860a05254c37044a11ca4d2c011d642c6bff9a98
chrysante <gruenlilie@protonmail.com>
2025-04-20 12:02:52 +0200

    feat: improve messenger

--- include/utl/messenger.hpp
+++ include/utl/messenger.hpp
@@ -1,51 +1,104 @@
 #pragma once
 
+#include <any>
+#include <atomic>
+#include <concepts>
+#include <mutex>
+#include <typeindex>
+
 #include "functional.hpp"
 #include "hashmap.hpp"
 #include "scope_guard.hpp"
-#include "type_traits"
+#include "type_traits.hpp"
 #include "vector.hpp"
-
-#include <any>
-#include <typeindex>
 
 namespace utl {
 
-/// MARK: listener_id
+/// \brief Represents a unique ID used to identify and remove a registered
+/// listener.
 struct listener_id {
     std::type_index __utl_type;
     std::size_t __utl_index;
 };
 
-/// MARK: messenger
+/// \brief Asynchronous messenger for type-erased message dispatch and listener
+/// management.
+/// \note Thread-safe for concurrent calls to `send()`/`listen()`/`unlisten()`.
+/// \warning Do not call `unlisten()` from within a listener callback.
 class messenger {
 public:
-    void send(std::any&& message) { send_impl(std::move(message)); }
-    void send(std::any const& message) { send_impl(message); }
+    /// \brief Immediately invokes all listeners for the message's type.
+    void send(std::any&& message) {
+        std::lock_guard lock(_listener_mutex);
+        send_impl_unsynchronized(std::move(message));
+    }
 
-    template <typename T>
-    listener_id listen(std::invocable<T> auto&& listener) {
+    /// \overload
+    void send(std::any const& message) {
+        std::lock_guard lock(_listener_mutex);
+        send_impl_unsynchronized(message);
+    }
+
+    /// \brief Registers a listener for messages of type `T`.
+    /// \tparam T Message type.
+    /// \tparam L Invocable listener type.
+    /// \pre Not called from within a listener callback.
+    template <typename T, std::invocable<T> L>
+    listener_id listen(L&& listener) {
         using U = std::decay_t<T>;
         std::type_index const type = typeid(U);
+        std::lock_guard lock(_listener_mutex);
         auto& typed_listeners = _listeners[type];
         typed_listeners[_index] =
-            [listener = UTL_FORWARD(listener)](std::any const& message) {
+            [listener = std::forward<L>(listener)](std::any const& message) {
             std::invoke(listener, std::any_cast<U const&>(message));
         };
         return listener_id{ type, _index++ };
     }
 
+    /// \brief Registers a listener by inferring its argument type.
+    /// \tparam L Callable taking exactly one argument.
+    /// \pre Not called from within a listener callback.
     template <typename L>
     listener_id listen(L&& listener)
-    requires(utl::function_traits<L>::argument_count == 1) &&
-            requires { typename utl::function_traits<L>::template argument<0>; }
+    requires(utl::function_traits<L>::argument_count == 1)
     {
         using T = std::decay_t<
             typename utl::function_traits<L>::template argument<0>>;
-        return listen<T>(UTL_FORWARD(listener));
+        return listen<T>(std::forward<L>(listener));
     }
 
-    void unlisten(listener_id const& id) {
+    /// \brief Removes a registered listener.
+    /// \pre Not called from within a listener callback.
+    void unlisten(listener_id const& ID) {
+        std::lock_guard lock(_listener_mutex);
+        unlisten_unsynchronized(ID);
+    }
+
+    /// \brief Removes multiple listeners.
+    /// \pre Not called from within a listener callback.
+    template <std::input_iterator I, std::sentinel_for<I> S>
+    requires std::convertible_to<std::iter_value_t<I>, listener_id>
+    void unlisten(I begin, S end) {
+        std::lock_guard lock(_listener_mutex);
+        for (; begin != end; ++begin)
+            unlisten_unsynchronized(*begin);
+    }
+
+private:
+    friend class buffered_messenger;
+
+    /// \brief Internal dispatch, assumes caller holds `_listener_mutex`.
+    template <typename T>
+    void send_impl_unsynchronized(T&& message) {
+        std::type_index const type = message.type();
+        auto const& typed_listeners = _listeners[type];
+        for (auto&& [key, listener] : typed_listeners)
+            listener(message);
+    }
+
+    /// \brief Internal dispatch, assumes caller holds `_listener_mutex`.
+    void unlisten_unsynchronized(listener_id const& id) {
         auto const list_itr = _listeners.find(id.__utl_type);
         __utl_expect(list_itr != _listeners.end(), "Invalid ID");
         auto& typed_listeners = list_itr->second;
@@ -54,59 +107,67 @@ public:
         typed_listeners.erase(itr);
     }
 
-private:
-    template <typename T>
-    void send_impl(T&& message) {
-        std::type_index const type = message.type();
-        auto const& typed_listeners = _listeners[type];
-        for (auto&& [key, listener] : typed_listeners) {
-            listener(message);
-        }
-    }
-
-private:
+    std::recursive_mutex _listener_mutex;
     hashmap<std::type_index,
             hashmap<std::size_t, function<void(std::any const&)>>>
         _listeners;
     std::size_t _index = 0;
 };
 
-/// MARK: buffered_messenger
+/// \brief Buffered variant of messenger.
+/// Messages are queued and only dispatched when `flush()` is called.
+/// \note Thread-safe. May be flushed from any thread.
+/// \warning Sending a message during `flush()` defers the new message to the
+/// next flush.
 class buffered_messenger: private messenger {
 public:
-    void send(std::any const& message) { _messages.push_back(message); }
-
-    void send(std::any&& message) { _messages.push_back(std::move(message)); }
-
-    void send_now(std::any const& message) { messenger::send(message); }
-
-    void send_now(std::any&& message) { messenger::send(std::move(message)); }
-
     using messenger::listen;
     using messenger::unlisten;
 
-    void flush() {
-        // guard this here to prevent feedback loops
-        if (flushing) {
-            return;
-        }
-        flushing = true;
-        scope_guard reset = [&] { flushing = false; };
-        for (auto const& msg : _messages) {
-            messenger::send(msg);
-        }
-        _messages.clear();
+    /// \brief Queues a message to be sent on next flush.
+    void send(std::any const& message) {
+        std::lock_guard lock(_message_queue_mutex);
+        _messages.push_back(message);
     }
 
-    std::size_t buffered_messages() const { return _messages.size(); }
+    /// \overload
+    void send(std::any&& message) {
+        std::lock_guard lock(_message_queue_mutex);
+        _messages.push_back(std::move(message));
+    }
+
+    /// \brief Immediately invokes all listeners for the message.
+    void send_now(std::any const& message) { messenger::send(message); }
+
+    /// \overload
+    void send_now(std::any&& message) { messenger::send(std::move(message)); }
+
+    /// \brief Dispatches all buffered messages in FIFO order.
+    /// \note Reentrant-safe: returns immediately if another thread is already
+    /// flushing.
+    void flush() {
+        if (!_flush_mutex.try_lock())
+            return;
+        std::lock_guard flush_lock(_flush_mutex, std::adopt_lock);
+        std::unique_lock msg_lock(_message_queue_mutex);
+        auto msgs = std::move(_messages);
+        _messages.clear();
+        msg_lock.unlock();
+        std::lock_guard listener_lock(this->_listener_mutex);
+        for (auto& msg : msgs)
+            messenger::send_impl_unsynchronized(std::move(msg));
+    }
 
 private:
+    std::mutex _flush_mutex;
+    std::mutex _message_queue_mutex;
     utl::vector<std::any> _messages;
-    bool flushing = false;
 };
 
-/// Convinience Classes
-/// MARK: receiver
+/// \brief Non-owning wrapper for receiving messages from a messenger.
+/// Automatically unregisters all listeners on destruction.
+/// \note Not thread-safe. Must not be accessed from multiple threads
+/// concurrently.
 template <typename Messenger>
 class receiver {
     static_assert(std::is_same_v<Messenger, messenger> ||
@@ -121,36 +182,39 @@ public:
     receiver& operator=(receiver&&) = default;
     ~receiver() { unlisten_all(); }
 
-    void set_messenger(std::weak_ptr<Messenger> m) {
+    /// \brief Sets the target messenger and clears existing listeners.
+    void set_messenger(std::weak_ptr<Messenger> msgn) {
         unlisten_all();
-        m = std::move(m);
+        m = std::move(msgn);
     }
 
-    template <typename T>
-    void listen(std::invocable<T> auto&& listener) {
-        __utl_expect(m != nullptr);
-        listen_impl(UTL_FORWARD(listener));
+    /// \brief Registers a listener for a specific type.
+    /// \note Does nothing if the messenger has expired.
+    template <typename T, std::invocable<T> L>
+    void listen(L&& listener) {
+        listen_impl(std::forward<L>(listener));
     }
 
+    /// \brief Registers a listener by inferring its argument type.
+    /// \note Does nothing if the messenger has expired.
     template <typename L>
     void listen(L&& listener) {
-        listen_impl(UTL_FORWARD(listener));
+        listen_impl(std::forward<L>(listener));
     }
 
+    /// \brief Removes all registered listeners.
     void unlisten_all() {
-        auto const msg = m.lock();
-        for (auto const& id : ids) {
-            msg->unlisten(id);
-        }
+        if (auto msgn = m.lock())
+            msgn->unlisten(ids.begin(), ids.end());
         ids.clear();
     }
 
 private:
-    void listen_impl(auto&& l) {
+    template <typename L>
+    void listen_impl(L&& l) {
         auto const msg = m.lock();
-        if (msg) {
-            ids.push_back(msg->listen(UTL_FORWARD(l)));
-        }
+        if (msg)
+            ids.push_back(msg->listen(std::forward<L>(l)));
     }
 
 private:
@@ -158,7 +222,7 @@ private:
     utl::vector<listener_id> ids;
 };
 
-/// MARK: emitter
+/// \brief Wrapper for sending messages to a `messenger`.
 template <typename Messenger>
 class emitter {
     static_assert(std::is_same_v<Messenger, messenger> ||
@@ -166,36 +230,63 @@ class emitter {
 
 public:
     emitter() = default;
+
     explicit emitter(std::shared_ptr<Messenger> m): m(std::move(m)) {}
 
+    /// \brief Sets the target messenger.
     void set_messenger(std::shared_ptr<Messenger> m) {
         __utl_expect(m);
         m = std::move(m);
     }
 
+    /// \brief Sends a message (buffered or immediate depending on messenger).
     void send(std::any&& message) const {
         __utl_expect(m);
         m->send(std::move(message));
     }
 
+    /// \overload
     void send(std::any const& message) const {
         __utl_expect(m);
         m->send(message);
     }
 
+    /// \brief Immediately dispatches a message, bypassing the buffer.
     void send_now(std::any&& message) const {
-        __utl_expect(m);
-        m->send_now(std::move(message));
+        send_now_impl(std::move(message));
     }
 
-    void send_now(std::any const& message) const {
-        __utl_expect(m);
-        m->send_now(message);
-    }
+    /// \overload
+    void send_now(std::any const& message) const { send_now_impl(message); }
 
 private:
+    template <typename T>
+    void send_now_impl(T&& message) const {
+        __utl_expect(m);
+        if constexpr (std::same_as<Messenger, buffered_messenger>)
+            m->send_now(std::forward<T>(message));
+        else
+            m->send(std::forward<T>(message));
+    }
+
     std::shared_ptr<Messenger> m;
-    utl::vector<listener_id> ids;
+};
+
+/// \brief Combines `receiver` and `emitter` for bidirectional messaging.
+/// \note `receiver`-interface not thread-safe. Must not be accessed from
+/// multiple threads concurrently.
+template <typename Messenger>
+class transceiver: public receiver<Messenger>, public emitter<Messenger> {
+public:
+    transceiver() = default;
+    explicit transceiver(std::shared_ptr<Messenger> m):
+        receiver<Messenger>(m), emitter<Messenger>(m) {}
+
+    /// \brief Sets the shared messenger for both sending and receiving.
+    void set_messenger(std::shared_ptr<Messenger> m) {
+        receiver<Messenger>::set_messenger(m);
+        emitter<Messenger>::set_messenger(m);
+    }
 };
 
 } // namespace utl
--- test/utl/messenger.t.cpp
+++ test/utl/messenger.t.cpp
@@ -1,81 +1,140 @@
+#include <thread>
+
 #include <catch2/catch_test_macros.hpp>
+
 #include <utl/messenger.hpp>
 
-TEST_CASE("messenger") {
+TEST_CASE("test messenger", "[messenger]") {
     utl::messenger m;
-
     int reference = 0;
     int reciever_count = 0;
-
     auto l1 = m.listen<int>([&](int i) {
         CHECK(i == reference);
         ++reciever_count;
     });
-
     reference = 1;
     m.send(1);
-
-    // notice no <int>
     auto l2 = m.listen([&](int i) {
         CHECK(i == reference);
         ++reciever_count;
     });
-
     CHECK(reciever_count == 1);
     reciever_count = 0;
-
     reference = 2;
     m.send(2);
     CHECK(reciever_count == 2);
     reciever_count = 0;
-
     m.unlisten(l2);
-
     reference = 3;
     m.send(3);
     CHECK(reciever_count == 1);
     reciever_count = 0;
-
     m.unlisten(l1);
-
     reference = 4;
     m.send(4);
     CHECK(reciever_count == 0);
     reciever_count = 0;
 }
 
-TEST_CASE("buffered_messenger") {
+TEST_CASE("test buffered_messenger", "[messenger]") {
     utl::buffered_messenger m;
-
     int testValue = 0;
-
     auto l = m.listen([&](int msg) { testValue = msg; });
-
     m.send(-1);
     CHECK(testValue == 0);
     m.flush();
     CHECK(testValue == -1);
-    CHECK(m.buffered_messages() == 0);
-
     m.unlisten(l);
-
     m.send(-2);
     m.flush();
     CHECK(testValue == -1);
 }
 
-TEST_CASE("emitter/receiver") {
+TEST_CASE("test emitter/receiver", "[messenger]") {
     auto m = std::make_shared<utl::buffered_messenger>();
-
     utl::emitter<utl::buffered_messenger> e(m);
     utl::receiver<utl::buffered_messenger> r(m);
-
     int value = 0;
-
     r.listen([&](int i) { value = i; });
-
     e.send(-1);
     CHECK(value == 0);
     m->flush();
     CHECK(value == -1);
+}
+
+TEST_CASE("recursive send", "[messenger]") {
+    utl::messenger m;
+    bool second_called = false;
+    enum Message { First, Second };
+    auto id = m.listen([&](Message msg) {
+        if (msg == First)
+            m.send(Second);
+        else if (msg == Second)
+            second_called = true;
+    });
+    m.send(First);
+    CHECK(second_called);
+}
+
+TEST_CASE("concurrent access", "[messenger]") {
+    utl::messenger m;
+    std::atomic<bool> running = true;
+    std::atomic<int> counter = 0;
+    enum Message { Ping };
+    std::thread sender([&] {
+        while (running) {
+            m.send(Ping);
+            std::this_thread::yield();
+        }
+    });
+    std::thread listener_manager([&] {
+        while (running) {
+            auto id = m.listen([&](Message msg) { counter++; });
+            m.unlisten(id);
+            std::this_thread::yield();
+        }
+    });
+    while (counter < 500)
+        std::this_thread::yield();
+    running = false;
+    sender.join();
+    listener_manager.join();
+    CHECK(counter >= 500);
+}
+
+TEST_CASE("parallel flush", "[messenger]") {
+    auto m = std::make_shared<utl::buffered_messenger>();
+    std::atomic<int> count = 0;
+    utl::receiver<utl::buffered_messenger> r(m);
+    enum class Message : int;
+    r.listen([&](Message) { ++count; });
+    m->send(Message{});
+    m->send(Message{});
+    std::thread flush1([&] { m->flush(); });
+    std::thread flush2([&] { m->flush(); });
+    flush1.join();
+    flush2.join();
+    CHECK(count == 2);
+}
+
+TEST_CASE("send from buffered message", "[messenger]") {
+    auto m = std::make_shared<utl::buffered_messenger>();
+    utl::receiver<utl::buffered_messenger> r(m);
+    enum Message { First, Second, Third };
+    int count = 0;
+    r.listen([&](Message msg) {
+        if (msg == First)
+            m->send(Second);
+        if (msg == Second)
+            m->send_now(Third);
+    });
+    r.listen([&](Message msg) { ++count; });
+    m->send(First);
+    CHECK(count == 0);
+    m->flush();
+    CHECK(count == 1);
+    m->flush();
+    CHECK(count == 3);
+    m->flush();
+    CHECK(count == 3);
 }
